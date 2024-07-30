e72d70be342b9cc644c6e8ace18248dcbe26a194
iskandar <iskandarovi542@gmail.com>
2024-07-30 12:31:32 +0500

    CiCd

--- lib/views/screens/splash_screen/splash_screen.dart
+++ lib/views/screens/splash_screen/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:qr_scaner/views/screens/onboarding/onboarding1.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,20 +8,20 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) {
-            return const Onboarding1();
-          },
-        ),
-      );
-    });
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Future.delayed(const Duration(seconds: 5), () {
+  //     Navigator.push(
+  //       context,
+  //       MaterialPageRoute(
+  //         builder: (context) {
+  //           return const Onboarding1();
+  //         },
+  //       ),
+  //     );
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
